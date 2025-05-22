0dab677ba905360def0f7fee138d35486ffb3431
Ash0508 <singhankush0508@gmail.com>
2025-05-22 17:07:26 +0530

    changes in firebase admin

--- backend/firebase/firebaseConfig.js
+++ backend/firebase/firebaseConfig.js
@@ -1,9 +1,18 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+require("dotenv").config();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
+  console.log("✅ Firebase Admin Initialized");
+} catch (err) {
+  console.error("❌ Firebase Admin Init Error:", err);
+}
 
 const db = admin.firestore();
 module.exports = db;
